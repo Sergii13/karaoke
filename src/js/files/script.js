21179a9053511e6e9b7d9c92d938e1a22f73b354
sergii13 <sergii.khilchuk13@gmail.com>
2023-04-11 16:44:29 +0300

    fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,23 +1,18 @@
-// Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
-// Підключення списку активних модулів
-import { flsModules } from "./modules.js";
 import { gotoBlock } from "./scroll/gotoblock.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-  const wrapper = document.querySelector(".wrapper");
-  const header = document.querySelector("header");
-  setPaddingWrapper(header, wrapper);
-  window.addEventListener("resize", () => {
-    console.log("sasa");
-    setPaddingWrapper(header, wrapper);
-  });
-});
-
-function setPaddingWrapper(header, wrapper) {
-  const headerHeight = header.offsetHeight;
-  wrapper.style.paddingTop = headerHeight + "px";
-}
+// window.addEventListener("DOMContentLoaded", () => {
+//   const wrapper = document.querySelector(".wrapper");
+//   const header = document.querySelector("header");
+//   setPaddingWrapper(header, wrapper);
+//   window.addEventListener("resize", () => {
+//     setPaddingWrapper(header, wrapper);
+//   });
+// });
+//
+// function setPaddingWrapper(header, wrapper) {
+//   const headerHeight = header.offsetHeight;
+//   wrapper.style.paddingTop = headerHeight + "px";
+// }
 
 const btnDown = document.querySelector(".main-block__scroll");
 const sections = document.querySelectorAll("section");
@@ -28,7 +23,6 @@ if (btnDown) {
   } else {
     btnDown.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log(sections[1]);
       gotoBlock(sections[1], true, 500, 0);
     });
   }
@@ -37,7 +31,6 @@ const customInput = document.querySelector("[data-custom]");
 if (customInput) {
   customInput.addEventListener("focus", () => {
     customInput.parentElement.querySelector("input[type=radio]").checked = true;
-    console.log("");
   });
 }
 let input = document.querySelector("[data-custom]");
